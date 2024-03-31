23545e0ea33931fb1dc66fbc73cc313dfc50deec
8505code <sfwwslm@gmail.com>
2024-03-31 08:10:48 +0000

    增加操作目录相关功能

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='czo',
-    version='0.1.3',
+    version='0.1.5',
     description='构建测试数据的工具包',
     long_description='伪造信息和随机数据',
     author='sfwwslm',
@@ -10,7 +10,7 @@ setup(
     url='https://github.com/sfwwslm/czo.git',
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=[],
+    install_requires=['setuptools>=69.2.0', 'wheel'],
     keywords='test faker rand',
-    python_requires=">=2.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*"
+    python_requires=">=3.10.*"
 )
--- src/czo/__init__.py
+++ src/czo/__init__.py
@@ -3,7 +3,7 @@
 """
 from ._date import FastDate
 from ._faker import Faker
-from ._path import Path
+from ._path import DirLib, PathLib
 from ._rand import Random
 
-__all__ = ["FastDate", "Faker", "Path", "Random"]
+__all__: list[str] = ["FastDate", "Faker", "PathLib", "DirLib", "Random"]
