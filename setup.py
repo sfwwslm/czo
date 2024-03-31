06a8a7fc326927ad3333ecb24cd5245d09e52b5b
8505code <sfwwslm@gmail.com>
2024-03-31 08:15:32 +0000

    修复版本依赖格式错误

--- setup.py
+++ setup.py
@@ -6,8 +6,6 @@ setup(
     description='构建测试数据的工具包',
     long_description='伪造信息和随机数据',
     author='sfwwslm',
-    author_email='sfwwslm@gmail.com',
-    url='https://github.com/sfwwslm/czo.git',
     packages=find_packages("src"),
     package_dir={"": "src"},
     install_requires=['setuptools>=69.2.0', 'wheel'],
