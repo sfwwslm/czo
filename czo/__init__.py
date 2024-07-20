7028c3ca2a5bfa7f397f6799d1394e1617de13e1
sfwwslm <sfwwslm@gmail.com>
2024-07-20 11:00:40 +0800

    新增功能，未来替换 Faker

--- czo/__init__.py
+++ czo/__init__.py
@@ -5,7 +5,8 @@ from ._date import DateLib
 from ._faker import Faker
 from ._net import NetLib
 from ._path import DirLib, PathLib
+from ._person import Person
 from ._rand import Random
 
 __all__: list[str] = ["DateLib", "Faker",
-                      "PathLib", "DirLib", "Random", "NetLib"]
+                      "PathLib", "DirLib", "Random", "NetLib", "Person"]
