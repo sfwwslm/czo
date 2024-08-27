917c9147cee99a1fa66a369ca64844fccf393ca0
sfwwslm <sfwwslm@gmail.com>
2024-08-27 14:04:55 +0800

    1.重构判断是否在ip范围方法 2.另一种风格格式化代码

--- czo/_net.py
+++ czo/_net.py
@@ -1,7 +1,7 @@
 import ipaddress
 from ipaddress import IPv4Address, IPv6Address
 from time import sleep
-from typing import Any, Generator
+from typing import Any, Generator, Union
 
 from .utils import get_methods_and_properties_with_docs
 
@@ -32,25 +32,37 @@ class NetLib:
         """
 
         try:
-            ip_type: ipaddress.IPv6Network | ipaddress.IPv4Network = ipaddress.IPv6Network if ':' in address else ipaddress.IPv4Network
-            cidr = ip_type(f'{address}/{netmask_or_prefix}')
+            ip_type: ipaddress.IPv6Network | ipaddress.IPv4Network = (
+                ipaddress.IPv6Network if ":" in address else ipaddress.IPv4Network
+            )
+            cidr = ip_type(f"{address}/{netmask_or_prefix}")
 
             # 输出网络地址、子网掩码、广播地址等信息
             host_count = cidr.num_addresses - 2
-            range = str(cidr.network_address + 1) + '-' + \
-                str(cidr.broadcast_address - 1)
+            range = (
+                str(cidr.network_address + 1) + "-" + str(cidr.broadcast_address - 1)
+            )
             network_address = str(cidr.network_address)
             broadcast_address = str(cidr.broadcast_address)
             netmask = str(cidr.netmask)
             prefixlen = cidr.prefixlen
 
-            return {'host_count': host_count, 'range': range, 'network_address': network_address, 'broadcast_address': broadcast_address, 'prefixlen': prefixlen, 'netmask': netmask}
+            return {
+                "host_count": host_count,
+                "range": range,
+                "network_address": network_address,
+                "broadcast_address": broadcast_address,
+                "prefixlen": prefixlen,
+                "netmask": netmask,
+            }
 
         except ValueError as e:
             return e
 
     @staticmethod
-    def ipaddress_generator(number: int, max: int | None = None, is_ipv6: bool = False,  **kwargs) -> Generator[list[str], Any, None]:
+    def ipaddress_generator(
+        number: int, max: int | None = None, is_ipv6: bool = False, **kwargs
+    ) -> Generator[list[str], Any, None]:
         """
         生成指定数量的IP地址序列。
 
@@ -89,21 +101,20 @@ class NetLib:
             d_range: int = 255
             ip_list: list = []
             count: int = 0
-            for _ in range(1, max+1):
-                ip_list.append(ipaddress.IPv4Address(
-                    f"{a}.{b}.{c}.{d}").compressed)
+            for _ in range(1, max + 1):
+                ip_list.append(ipaddress.IPv4Address(f"{a}.{b}.{c}.{d}").compressed)
                 count += 1
                 d += 1
 
                 if d == d_range:
                     d = 1
-                    c = c+1
+                    c = c + 1
                     if c == c_range:
                         c = 1
-                        b = b+1
+                        b = b + 1
                         if b == b_range:
                             b = 1
-                            a = a+1
+                            a = a + 1
                             if a == a_range:
                                 a = 1
 
@@ -123,33 +134,36 @@ class NetLib:
             h_range: int = 65536
             ip_list: list = []
             count: int = 0
-            for _ in range(1, max+1):
-                ip_list.append(ipaddress.IPv6Address(
-                    f"{a:04x}:{b:04x}:{c:04x}:{d:04x}:{e:04x}:{f:04x}:{g:04x}:{h:04x}").compressed)
+            for _ in range(1, max + 1):
+                ip_list.append(
+                    ipaddress.IPv6Address(
+                        f"{a:04x}:{b:04x}:{c:04x}:{d:04x}:{e:04x}:{f:04x}:{g:04x}:{h:04x}"
+                    ).compressed
+                )
                 count += 1
                 h += 1
 
                 if h == h_range:
                     h = 1
-                    g = g+1
+                    g = g + 1
                     if g == g_range:
                         g = 1
-                        f = f+1
+                        f = f + 1
                         if f == f_range:
                             f = 1
-                            e = e+1
+                            e = e + 1
                             if e == e_range:
                                 e = 1
-                                d = d+1
+                                d = d + 1
                                 if d == d_range:
                                     d = 1
-                                    c = c+1
+                                    c = c + 1
                                     if c == c_range:
                                         c = 1
-                                        b = b+1
+                                        b = b + 1
                                         if b == b_range:
                                             b = 1
-                                            a = a+1
+                                            a = a + 1
                                             if a == a_range:
                                                 a = 1
 
@@ -160,20 +174,26 @@ class NetLib:
                     yield ip_list
 
     @staticmethod
-    def generate_ip_list(number: int, *, is_ipv6: bool = False, ip_str: str | None = None, v6_exploded: bool = False) -> list[str]:
+    def generate_ip_list(
+        number: int,
+        *,
+        is_ipv6: bool = False,
+        ip_str: str | None = None,
+        v6_exploded: bool = False,
+    ) -> list[str]:
         """
         生成指定数量的IP地址字符串列表。
 
-        Args:
+        参数:
         - number: 生成IP地址的数量。
         - is_ipv6: 指示是否生成IPv6地址。
         - ip_str: 指定IP地址字符串，如果提供了该参数，则使用该字符串作为起始IP地址。
         - v6_exploded: 指示生成的IPv6地址是否以展开格式表示。
 
-        Returns:
+        返回值:
         - 一个包含生成的IP地址字符串的列表。
 
-        Examples:
+        示例:
         >>> print(NetLib.generate_ip_list(3, ip_str="192.168.1.1"))
 
         >>> print(NetLib.generate_ip_list(3, is_ipv6=True, ip_str="2001:db8::1"))
@@ -196,55 +216,57 @@ class NetLib:
         return [ipaddress.IPv4Address(ipv4 + i).compressed for i in range(number)]
 
     @staticmethod
-    def ip_in_subnet(ip_str, subnet_str):
-        """检查ip地址是否在指定的子网内。
+    def is_ip_in_range(
+        ip_str: str,
+        subnet_str: Union[str, None] = None,
+        *,
+        src_ip: str | None = None,
+        dst_ip: str | None = None,
+    ) -> bool:
+        """检查 IP 地址是否在指定的子网或 IP 范围内。
 
-        Examples:
-        >>> ip = '2a00::110:133'
-        >>> subnet = '2a00::110:0/120'
+        参数:
+        - ip_str: 要检查的 IP 地址。
+        - subnet_str: 可选的子网（CIDR 形式或 IP 范围，格式为 'start_ip-end_ip'）。
+        - src_ip: 可选的起始 IP 地址，当没有提供 `subnet_str` 时使用。
+        - dst_ip: 可选的结束 IP 地址，当没有提供 `subnet_str` 时使用。
 
-        >>> if ip_in_subnet(ip, subnet):
-        ...     print(f"{ip} 在子网 {subnet} 内")
-        ... else:
-        ...     print(f"{ip} 不在子网 {subnet} 内")
-        ...
+        返回值:
+        - 如果 IP 地址在指定范围内，则返回 True;否则返回 False。
+
+        示例:
+        - is_ip_in_range("192.168.1.10", "192.168.1.0/24")
+        - is_ip_in_range("192.168.1.10", "192.168.1.1-192.168.1.20")
+        - is_ip_in_range("192.168.1.10", src_ip="192.168.1.1", dst_ip="192.168.1.20")
+
+        - is_ip_in_range("2a00::110:1", "2a00::110:0/116")
+        - is_ip_in_range("2a00::110:1", "2a00::110:1-2a00::110:fff")
         """
+        ip: IPv4Address | IPv6Address = ipaddress.ip_address(ip_str)
 
-        # 将 IP 地址和子网掩码字符串转换为 IPv4Network 对象
-        ip_network = ipaddress.ip_network(subnet_str)
-        # 将 IP 地址字符串转换为 IPv4Address 对象
-        ip_address = ipaddress.ip_address(ip_str)
-        # 判断 IP 地址是否在子网内
-        return ip_address in ip_network
+        if subnet_str:
+            if "/" in subnet_str:
+                ip_network = ipaddress.ip_network(subnet_str, strict=False)
+                return ip in ip_network
+            elif "-" in subnet_str:
+                start_ip_str, end_ip_str = map(str.strip, subnet_str.split("-"))
+                start_ip, end_ip = ipaddress.ip_address(
+                    start_ip_str
+                ), ipaddress.ip_address(end_ip_str)
+                return start_ip <= ip <= end_ip
+
+        if src_ip and dst_ip:
+            start_ip, end_ip = ipaddress.ip_address(src_ip), ipaddress.ip_address(
+                dst_ip
+            )
+            return start_ip <= ip <= end_ip
+
+        return False
 
     @staticmethod
-    def ipv6_in_range(ip_str, start_str, end_str):
-        """检查ipv6地址是否在指定的范围内。
-
-        Examples:
-        >>> ip = '2a00::110:133'
-        >>> start = '2a00::110:13'
-        >>> end = '2a00::110:133'
-
-        >>> if ipv6_in_range(ip, start, end):
-        ...    print(f"{ip} 在范围 {start} - {end} 内")
-        ... else:
-        ...    print(f"{ip} 不在范围 {start} - {end} 内")
-        ...
-        """
-
-        # 将起始和结束地址字符串转换为 IPv6Address 对象
-        start_address = ipaddress.ip_address(start_str)
-        end_address = ipaddress.ip_address(end_str)
-        # 将要判断的地址字符串转换为 IPv6Address 对象
-        ip_address = ipaddress.ip_address(ip_str)
-        # 判断地址是否在范围内
-        return start_address <= ip_address <= end_address
-
-        # 示例用法
-
-    @staticmethod
-    def mac_generator(number: int, max: int = 10, **kwargs) -> Generator[list, Any, None]:
+    def mac_generator(
+        number: int, max: int = 10, **kwargs
+    ) -> Generator[list, Any, None]:
         """
         生成MAC地址
         """
@@ -262,25 +284,25 @@ class NetLib:
         f_range: int = 256
         count = 0
         mac_list = []
-        for _ in range(1, max+1):
+        for _ in range(1, max + 1):
             mac_list.append(f"{a:02x}:{b:02x}:{c:02x}:{d:02x}:{e:02x}:{f:02x}")
             count += 1
             f += 1
             if f == f_range:
                 f = 1
-                e = e+1
+                e = e + 1
                 if e == e_range:
                     e = 1
-                    d = d+1
+                    d = d + 1
                     if d == d_range:
                         d = 1
-                        c = c+1
+                        c = c + 1
                         if c == c_range:
                             c = 1
-                            b = b+1
+                            b = b + 1
                             if b == b_range:
                                 b = 1
-                                a = a+1
+                                a = a + 1
                                 if a == a_range:
                                     a = 1
             if len(mac_list) % number == 0:
