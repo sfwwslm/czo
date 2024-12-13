ec2be5184ba5d68a8060a7ea810613cfa629e5ad
sfwwslm <sfwwslm@gmail.com>
2024-12-13 17:16:09 +0800

    实例上也可以使用help方法

--- czo/utils/__init__.py
+++ czo/utils/__init__.py
@@ -19,7 +19,7 @@ def count_chr_length(text, no_ascii=2) -> int:
     return length
 
 
-def _get_methods_and_properties_with_docs(cls):
+def _get_methods_and_properties_with_docs(cls, *args, **kwargs):
     """通过这个函数，可以方便地获取类的所有公共方法和属性，并以一种格式化的方式输出它们的名称和文档注释。"""
     methods_and_properties: dict[str, str] = {}
 
@@ -84,5 +84,7 @@ def _get_methods_and_properties_with_docs(cls):
 
 def add_help(cls):
     """通过这个装饰器，可以方便地为类添加一个help 方法，该方法可以方便地获取类的所有公共方法和属性，并以一种格式化的方式输出它们的名称和文档注释。"""
-    cls.help = lambda: _get_methods_and_properties_with_docs(cls)
+    cls.help = lambda *args, **kwargs: _get_methods_and_properties_with_docs(
+        cls, *args, **kwargs
+    )
     return cls
